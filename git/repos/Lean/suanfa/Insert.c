4a0c7c7424bc01adfd1c26b53bfd3a6da9c6c888
sean <caizhenzhen2010@sina.com>
2015-09-19 08:05:48 +0800

    commit

--- git/repos/Lean/suanfa/Insert.c
+++ git/repos/Lean/suanfa/Insert.c
@@ -1,8 +1,12 @@
+//直接插入排序
+/*起始游标i选定第二个元素，如果i-1即第一个元素大于第二个元素，
+temp = a[i],游标j=i,将i-1后往后移动直到temp>a[j-1]。将temp插入到a[j-1]此时i=2之前的都是有序
+循环游标i直到数组最后一个元素。*/
 #include <stdio.h>
 int main(void)
 {
   int num = 0;
-  int array[10] = {15,8,2,16,9,54,52,11,25,3};
+  int array[10] = {13,2,5,18,7,12,9,10,15,6};
   int i,j,temp;
   for (i = 1;i <= 9;i++)
   {
@@ -20,7 +24,7 @@ int main(void)
     }
   }
   for (i = 0;i <= 9;i++)
-  printf("array: %d\n",array[i]);
+  printf("%d,",array[i]);
   printf("num=%d\n",num);
   printf("\n");
 }
